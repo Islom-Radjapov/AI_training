ea91240221d2f4319565b49faf9480630198d535
islom <radjapov_islom@mail.ru>
2022-11-22 17:06:38 +0500

    look plt

--- by_Islom/work.py
+++ by_Islom/work.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 # (Modelimiz) Tog'ri hisoblash uchun funtsiya
 def forward(x):
-    # print(w)
     return x * w
 
 # Xatolik (Loss) ning funtsiya
@@ -14,19 +13,31 @@ def loss(x, y):
 x_soat = [1.0, 2.0, 3.0]
 y_baho = [4.0, 5.0, 6.0]
 
+w_list = []
+mse_list  = []
+
 for w in np.arange(0.0, 4.1, 0.1):
-    print(w)
-    # print("w={:.1f}".format(w))
+    print("w={:.3f}".format(w))
     L_umum = 0
 
     for x_hb_qiym, y_hb_qiym in zip(x_soat, y_baho):
         y_hb_bash = forward(x_hb_qiym)
         L_hb_qiym = loss(x_hb_qiym, y_hb_qiym)
         L_umum += L_hb_qiym
-        # print("\t", "{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(x_hb_qiym, y_hb_qiym, y_hb_bash, L_hb_qiym))
+        print("\t", "{:.2f}, {:.2f}, {:.2f}, {:.2f}".format(x_hb_qiym, y_hb_qiym, y_hb_bash, L_hb_qiym))
 
     # Harbir malumot uchun MSE ni hisoblaymiz
-    # print("MSE=>", L_umum / len(x_soat))
+    print("MSE=>", L_umum / len(x_soat))
+    w_list.append(w)
+    mse_list.append(L_umum / len(x_soat))
+
+# Grafik natija
+plt.plot(w_list, mse_list)
+plt.ylabel('Loss')
+plt.xlabel('w')
+plt.show()
+
+
 
 # Training Data (O'rgatishdagi malumotlar)
 x = [1.0, 2.0, 3.0]
